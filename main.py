04e47e70c5ba018a432dceb99482775b66970aa3
Sagar Patil <sagarreddypatil@gmail.com>
2020-07-17 17:07:38 -0400

    Use sequential layers

--- main.py
+++ main.py
@@ -18,25 +18,29 @@ class params:
 class AE(nn.Module):
     def __init__(self):
         super(AE, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
-        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
-        self.pool = nn.MaxPool2d(2, 2)
 
-        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
-        self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
+        self.encoder = nn.Sequential(
+            nn.Conv2d(1, 16, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+            nn.Conv2d(16, 4, 3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2),
+        )
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose2d(4, 16, 2, stride=2),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, 2, stride=2),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-
-        x = F.relu(self.t_conv1(x))
-        x = F.sigmoid(self.t_conv2(x))
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
 
 
-is_cuda = True
+is_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if is_cuda else "cpu")
 print(f"Running on {device}")
 
