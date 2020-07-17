16fb4730d03da717558e3017aaa6d09925f52587
Sagar Patil <sagarreddypatil@gmail.com>
2020-07-17 15:13:18 -0400

    Make autoencoder work

--- main.py
+++ main.py
@@ -2,92 +2,41 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
+from torchvision.utils import make_grid
 from torchvision import datasets, transforms
 import numpy as np
 from tqdm import tqdm
 
 
 class params:
-    batch_size = 4096
+    batch_size = 8192
     latent_dim = 4
-    epochs = 25
-    lr = 1e-4
+    epochs = 100
+    lr = 5e-3
 
 
-class Flatten(nn.Module):
-    def forward(self, x):
-        return torch.flatten(x, 1)
+class AE(nn.Module):
+    def __init__(self):
+        super(AE, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)
+        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+        self.pool = nn.MaxPool2d(2, 2)
 
-
-class DeFlatten(nn.Module):
-    def __init__(self, out_size):
-        self.out_size = out_size
-
-    def forward(self, x):
-        return x.view(x.size(0), *self.out_size)
-
-
-class VAE(nn.Module):
-    def __init__(self, device, latent_dim):
-        super(VAE, self).__init__()
-        self.device = device
-        ksp = [3, 2, 1]  # kernel size, stride, padding
-
-        self.encoder_conv = nn.Sequential(
-            nn.Conv2d(1, 32, *ksp),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, *ksp),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-        )
-
-        self.decoder_conv = nn.Sequential(
-            nn.ConvTranspose2d(32, 32, 3, 2, 1, 1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.ConvTranspose2d(32, 1, 3, 2, 1, 1),
-            nn.BatchNorm2d(1),
-            nn.Sigmoid(),
-        )
-
-        self.fc1a = nn.Linear(7 * 7 * 32, latent_dim)
-        self.fc1b = nn.Linear(7 * 7 * 32, latent_dim)
-
-        self.fc2 = nn.Linear(latent_dim, 7 * 7 * 32)
-
-    def encode(self, x):
-        conv_out = self.encoder_conv(x)
-        out = torch.flatten(conv_out, 1)
-        return self.fc1a(out), self.fc1b(out)
-
-    def reparameterize(self, mu, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn(*std.size())
-
-        return mu + eps * std
-
-    def decode(self, z):
-        out = self.fc2(z)
-        out = out.view(out.size(0), 32, 7, 7)
-        return self.decoder_conv(out)
+        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
 
     def forward(self, x):
-        mu, logvar = self.encode(x)
-        z = self.reparameterize(mu, logvar)
-        return self.decode(z), mu, logvar
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = F.relu(self.t_conv1(x))
+        x = F.sigmoid(self.t_conv2(x))
+        return x
 
 
-def loss_function(recon_x, x, mu, logvar):
-    BCE = F.binary_cross_entropy(
-        recon_x.view(-1, 784), x.view(-1, 784), reduction="sum"
-    )
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
-
-    return BCE + KLD
-
-
-is_cuda = torch.cuda.is_available()
+is_cuda = True
 device = torch.device("cuda" if is_cuda else "cpu")
 print(f"Running on {device}")
 
@@ -105,12 +54,13 @@ test_loader = torch.utils.data.DataLoader(test_set, **kwargs)
 
 metric_names = ["loss"]
 
-model = VAE(device, params.latent_dim)
+model = AE()
 model.to(device)
 
 num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print(f"Parameters: {num_params}")
 
+criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=params.lr)
 
 writer = SummaryWriter(log_dir="./logs")
@@ -129,9 +79,9 @@ for epoch in range(params.epochs):
         data = data.to(device)
 
         optimizer.zero_grad()
-        output, mu, logvar = model(data)
+        output = model(data)
 
-        loss = loss_function(output, data, mu, logvar)
+        loss = criterion(output, data)
 
         loss.backward()
         optimizer.step()
@@ -144,12 +94,19 @@ for epoch in range(params.epochs):
 
     test_metrics = [0]
     model.eval()
-    with torch.no_grad():
-        for data in test_loader:
-            data = data.to(device)
-            output, mu, logvar = model(data)
 
-            test_metrics[0] += loss_function(output, data, mu, logvar).item()
+    test_outputs = []
+
+    with torch.no_grad():
+        for (data, _) in test_loader:
+            data = data.to(device)
+            output = model(data)
+
+            test_metrics[0] += criterion(output, data)
+            test_outputs = output
+
+    grid = make_grid(test_outputs[:64])
+    writer.add_image("test/reconstructed", grid, epoch)
 
     test_metrics = np.array(test_metrics) / len(test_loader)
     for name, val in zip(metric_names, test_metrics):
